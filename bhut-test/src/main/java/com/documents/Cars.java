036cb9a4ff574298b0d13c02e7a53fa894dcab71
mateusCoder <mateus.moraes0507@gmail.com>
2022-09-01 12:26:26 -0300

    Created Project

---
+++ bhut-test/src/main/java/com/documents/Cars.java
@@ -0,0 +1,16 @@
+package com.documents;
+
+public class Cars {
+
+    private Long _id;;
+
+    private String title;
+
+    private String brand;
+
+   private String price;
+
+   private String age;
+
+   private Integer __v;
+}
